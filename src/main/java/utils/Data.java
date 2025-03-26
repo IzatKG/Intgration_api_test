3d41192da4bb85639823c35db460035507cc17ce
Izat <kerimbaevazat@gmail.com>
2025-03-26 15:19:22 +0600

    added some changes

--- src/main/java/utils/Data.java
+++ src/main/java/utils/Data.java
@@ -1,13 +1,14 @@
 package utils;
 
+import com.github.javafaker.Faker;
 import entities.request.GoRequestBody;
 
 public class Data {
-
+static Faker faker= new Faker();
     public static GoRequestBody createRequestBody(){
         return GoRequestBody.builder()
-                .name("Maksyt1675")
-                .email("ouyp45oj@gmail.com")
+                .name(faker.name().firstName())
+                .email(faker.internet().emailAddress())
                 .gender("male")
                 .status("active")
                 .build();
